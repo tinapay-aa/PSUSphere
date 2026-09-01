277af71d042bac476ba490af0a912c05e64c55c8
tinapay-aa <183698624+tinapay-aa@users.noreply.github.com>
2026-09-01 23:09:37 +0800

    Fixed the pagination file as it was mangled from being pasted, Mark. Finished sections A-C of part '04 Django Views', which tasked us to load progile image, set up the Menu, and create an organization list view.

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -6,4 +6,10 @@ class HomePageView(ListView):
  model = Organization
  context_object_name = 'home'
  template_name = "home.html"
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginate_by = 5
 # Create your views here.
